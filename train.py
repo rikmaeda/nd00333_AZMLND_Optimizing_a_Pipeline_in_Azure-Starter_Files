e441764ba6dd41f12e476fff31aa4e5fcf5cc8a4
rikmaeda <rikmaeda@gmail.com>
2021-06-03 16:31:21 -0300

    cluster setup

--- train.py
+++ train.py
@@ -10,17 +10,13 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-ds = ### YOUR CODE HERE ###
+# Create TabularDataset using TabularDatasetFactory
+ds = TabularDatasetFactory.from_delimited_files("https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
 
 x, y = clean_data(ds)
 
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
+# Split data into train and test sets.
+x_train, x_test, y_train, y_test = train_test_split(x, y)
 
 run = Run.get_context()
 
@@ -50,7 +46,6 @@ def clean_data(data):
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     
-
 def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
